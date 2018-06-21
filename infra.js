3fad0fc09f379827b595461590c31dc2009e59dc
mihirk <mihir.khatwani@gmail.com>
2018-06-21 04:23:06 +0000

    Build [ci skip]

---
+++ infra.js
@@ -0,0 +1,92 @@
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+const request = require('superagent');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const serverURL = (path) => `${process.env.SERVER_URL !== undefined ? process.env.SERVER_URL : 'http://localhost:8021'}/${path}`;
+
+const createRule = (formUUID, type, ruleData, ruleDependencyUUID) =>
+    request.post(serverURL("rule"), {
+        ruleDependencyUUID: ruleDependencyUUID,
+        formUUID: formUUID,
+        type: type,
+        data: ruleData.metadata,
+        uuid: ruleData.uuid,
+        name: ruleData.name,
+        fnName: ruleData.fn.name
+    }).on('error', console.log).then((resp) => console.log("CREATED"));
+
+const postAllRules = (organisationName, ruleFilePath) => {
+    const compiler = webpack({
+        target: 'web',
+        entry: {
+            rules: ruleFilePath
+        },
+        output: {
+            filename: '[name].bundle.js',
+            libraryTarget: 'umd',
+            library: 'rulesConfig',
+            path: path.resolve(__dirname, 'dist')
+        },
+        plugins: [
+            new UglifyJsPlugin({
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                uglifyOptions: {
+                    ecma: 5,
+                    warnings: false,
+                    compress: true,
+                    mangle: true,
+                    keep_fnames: true,
+                    keep_classnames: true,
+                    output: {comments: false, beautify: false}
+                }
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            "presets": [
+                                [
+                                    "env"
+                                ]
+                            ],
+                            "plugins": [
+                                "transform-class-properties",
+                                "transform-export-extensions",
+                                "transform-decorators-legacy",
+                                "transform-es2015-destructuring"
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+    });
+    compiler.run((err, stats) => {
+        const rules = require('./dist/rules.bundle.js');
+        const rulesContent = String(fs.readFileSync(path.resolve(__dirname, 'dist') + '/rules.bundle.js'));
+        request
+            .post(serverURL("ruleDependency"), {
+                code: rulesContent,
+                hash: stats.hash
+            }).set("ORGANISATION-NAME", organisationName)
+            .then((response) => {
+                const registry = rules[Object.keys(rules).find(r => rules[r].registry !== undefined)].registry;
+                registry.getAll()
+                    .forEach(([ruleKey, rulesData]) => {
+                        rulesData.map(ruleData => createRule(ruleKey.formUUID, ruleKey.type, ruleData, response.text));
+                    })
+            });
+    });
+};
+
+
+// postAllRules("","./test/additional/Rulez.js");
+module.exports = {postAllRules};
