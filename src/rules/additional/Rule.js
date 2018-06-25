6296ba9189195b4271295eb67bba62e494de3426
mihirk <mihir.khatwani@gmail.com>
2018-06-25 16:04:53 +0530

    Renaming to execution order

--- src/rules/additional/Rule.js
+++ src/rules/additional/Rule.js
@@ -1,8 +1,8 @@
 const RuleRegistry = require('./RuleRegistry');
 
-const RuleFactory = (formUUID, type) => (uuid, name, order, metadata = {}) => {
+const RuleFactory = (formUUID, type) => (uuid, name, executionOrder, metadata = {}) => {
     return (fn) => {
-        const ruleData = {metadata: metadata, fn: fn, uuid: uuid, name: name, order: order};
+        const ruleData = {metadata: metadata, fn: fn, uuid: uuid, name: name, executionOrder: executionOrder};
         RuleRegistry.add(formUUID, type, ruleData);
         fn.registry = RuleRegistry;
     };
