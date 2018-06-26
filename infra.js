b2ae87f041b9a46938d96af75b58d2e58eaeabb6
mihirk <mihir.khatwani@gmail.com>
2018-06-26 11:52:36 +0530

    Adding organisation name to rule creation

--- infra.js
+++ infra.js
@@ -6,7 +6,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const serverURL = (path) => `${process.env.SERVER_URL !== undefined ? process.env.SERVER_URL : 'http://localhost:8021'}/${path}`;
 
-const createRule = (formUUID, type, ruleData, ruleDependencyUUID) =>
+const createRule = (organisationName, formUUID, type, ruleData, ruleDependencyUUID) =>
     request.post(serverURL("rule"), {
         ruleDependencyUUID: ruleDependencyUUID,
         formUUID: formUUID,
@@ -16,7 +16,9 @@ const createRule = (formUUID, type, ruleData, ruleDependencyUUID) =>
         name: ruleData.name,
         fnName: ruleData.fn.name,
         executionOrder: ruleData.executionOrder
-    }).on('error', console.log)
+    })
+        .set("ORGANISATION-NAME", organisationName)
+        .on('error', console.log)
         .then(() => console.log(`Created Rule: ${ruleData.name} ${ruleData.fn.name}`));
 
 const postAllRules = (organisationName, ruleFilePath) => {
@@ -86,7 +88,7 @@ const postAllRules = (organisationName, ruleFilePath) => {
                 const registry = rules[Object.keys(rules).find(r => rules[r].registry !== undefined)].registry;
                 registry.getAll()
                     .forEach(([ruleKey, rulesData]) => {
-                        rulesData.map(ruleData => createRule(ruleKey.formUUID, ruleKey.type, ruleData, response.text));
+                        rulesData.map(ruleData => createRule(organisationName, ruleKey.formUUID, ruleKey.type, ruleData, response.text));
                     })
             });
     });
@@ -94,7 +96,7 @@ const postAllRules = (organisationName, ruleFilePath) => {
 
 const postRulesWithoutDependency = (organisationName, rules) => {
     rules.forEach(([ruleKey, rulesData]) => {
-        rulesData.map(ruleData => createRule(ruleKey.formUUID, ruleKey.type, ruleData));
+        rulesData.map(ruleData => createRule(organisationName, ruleKey.formUUID, ruleKey.type, ruleData));
     })
 };
 
