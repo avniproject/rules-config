bab81a45c3946d8ea2b851e6c52e6680c0d968b9
A.N.Sidtharthan <sidthunagarajan@gmail.com>
2018-07-11 19:37:08 +0530

    Code Format

--- src/rules/builder/StatusBuilderAnnotationFactory.js
+++ src/rules/builder/StatusBuilderAnnotationFactory.js
@@ -1,16 +1,16 @@
 const FormElementStatusBuilder = require('./FormElementStatusBuilder');
 
-const StatusBuilderAnnotationFactory = function(...argNames) {
-    const argToContextMapper = (args)=>{
+const StatusBuilderAnnotationFactory = function (...argNames) {
+    const argToContextMapper = (args) => {
         const contextMap = {};
-        argNames.forEach((argName,index)=>{
+        argNames.forEach((argName, index) => {
             contextMap[argName] = args[index];
         });
         return contextMap;
     }
-    return function(target, key, descriptor) {
+    return function (target, key, descriptor) {
         const oldFn = descriptor.value;
-        descriptor.value = function(...args) {
+        descriptor.value = function (...args) {
             const statusBuilder = new FormElementStatusBuilder(argToContextMapper(args));
             return oldFn(args, statusBuilder) || statusBuilder.build();
         }
