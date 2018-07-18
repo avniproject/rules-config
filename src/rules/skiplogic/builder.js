78173d817f8a98d45093f7273f546a78c3e78027
A.N.Sidtharthan <sidthunagarajan@gmail.com>
2018-07-18 22:06:05 +0530

    Make skiplogic builder generate classes with `ES6 class, annotation syntax` to keep it consistent with existing usage.

--- src/rules/skiplogic/builder.js
+++ src/rules/skiplogic/builder.js
@@ -12,22 +12,22 @@ const generalStatusBuilder = ({ context, findingStrategy, inputForStrategy, oper
 
 /* builds the `class {}` with `exec` method. `exec` method has access to all formElementFunctions built from filters.rules */
 const build = function (filters) {
-    const constructor = () => { };
     const generatedFunctions = {};
     filters.rules.forEach((rule) => {
         rule.build().forEach(({ fnName, fn }) => {
             generatedFunctions[fnName] = fn;
         });
     });
-
     const formElementFunctions = Object.assign({}, generatedFunctions, filters.directFunctions);
-    constructor.exec = (programEncounter, formElementGroup, today) => {
-        return FormElementsStatusHelper.getFormElementsStatusesWithoutDefaults(formElementFunctions, programEncounter, formElementGroup, today);
-    };
 
-    //registration by uuid
-    RuleFactory(filters.formUuid, 'ViewFilter')(filters.uuid, filters.name, filters.execOrder, filters.otherData, filters.uuid)(constructor);
-    return constructor;
+    @RuleFactory(filters.formUuid, 'ViewFilter')(filters.uuid, filters.name, filters.execOrder, filters.otherData, filters.uuid)
+    class ViewFilter {
+        static uuid = filters.uuid;
+        static exec(entity, formElementGroup, today) {
+            return FormElementsStatusHelper.getFormElementsStatusesWithoutDefaults(formElementFunctions, entity, formElementGroup, today);
+        }
+    }
+    return ViewFilter;
 };
 
 const buildAndExport = (filters, exprts) => {
